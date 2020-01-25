01bfdaa63fe0c5fb80b15a3332a8ab3939e18d47
Jason Watson <jason.jw@live.co.uk>
2020-01-25 16:26:50 +0100

    Remove pybind subclass - use function instead

--- interfaces/pybind/WaveformDataPacket.cc
+++ interfaces/pybind/WaveformDataPacket.cc
@@ -11,53 +11,45 @@ namespace interfaces {
 
 namespace py = pybind11;
 
-class WaveformDataPacketPy : public WaveformDataPacket {
-public:
-    using WaveformDataPacket::WaveformDataPacket;
-
-    py::array_t<uint8_t> GetDataPacketBuffer() {
-        return py::array_t<uint8_t>(GetPacketSize(), GetDataPacket(), py::cast(*this));
-    }
-};
+py::array_t<uint8_t> GetDataPacket(const WaveformDataPacket& packet) {
+    return py::array_t<uint8_t>(packet.GetPacketSize(), packet.GetDataPacket(), py::cast(packet));
+}
 
 void waveform_data_packet(py::module &m) {
-    // Enable the passing of the python-wrapped object back to C++
-    py::class_<WaveformDataPacket> datapacketbase(m, "_WaveformDataPacket");
-    py::class_<WaveformDataPacketPy, WaveformDataPacket> datapacket(m, "WaveformDataPacket");
-
+    py::class_<WaveformDataPacket> datapacket(m, "WaveformDataPacket");
     datapacket.def(py::init<size_t>());
-    datapacket.def("GetDataPacket", &WaveformDataPacketPy::GetDataPacketBuffer, py::return_value_policy::reference_internal);
-    datapacket.def("GetPacketSize", &WaveformDataPacketPy::GetPacketSize);
-    datapacket.def("GetNWaveforms", &WaveformDataPacketPy::GetNWaveforms);
-    datapacket.def("GetRegisterSetup", &WaveformDataPacketPy::GetRegisterSetup);
-    datapacket.def("IsLastSubPacket", &WaveformDataPacketPy::IsLastSubPacket);
-    datapacket.def("IsFirstSubPacket", &WaveformDataPacketPy::IsFirstSubPacket);
-    datapacket.def("GetNBuffers", &WaveformDataPacketPy::GetNBuffers);
-    datapacket.def("GetTACKTime", &WaveformDataPacketPy::GetTACKTime);
-    datapacket.def("GetEventNumber", &WaveformDataPacketPy::GetEventNumber);
-    datapacket.def("GetSlotID", &WaveformDataPacketPy::GetSlotID);
-    datapacket.def("GetDetectorID", &WaveformDataPacketPy::GetDetectorID);
-    datapacket.def("GetEventSequenceNumber", &WaveformDataPacketPy::GetEventSequenceNumber);
-    datapacket.def("GetDetectorUniqueTag", &WaveformDataPacketPy::GetDetectorUniqueTag);
-    datapacket.def("GetColumn", &WaveformDataPacketPy::GetColumn);
-    datapacket.def("GetStaleBit", &WaveformDataPacketPy::GetStaleBit);
-    datapacket.def("IsZeroSupressionEnabled", &WaveformDataPacketPy::IsZeroSupressionEnabled);
-    datapacket.def("GetRow", &WaveformDataPacketPy::GetRow);
-    datapacket.def("GetBlockPhase", &WaveformDataPacketPy::GetBlockPhase);
-    datapacket.def("GetCRC", &WaveformDataPacketPy::GetCRC);
-    datapacket.def("GetMBZ", &WaveformDataPacketPy::GetMBZ);
-    datapacket.def("IsTimeout", &WaveformDataPacketPy::IsTimeout);
-    datapacket.def("IsError", &WaveformDataPacketPy::IsError);
-    datapacket.def("GetWaveformSamplesNBlocks", &WaveformDataPacketPy::GetWaveformSamplesNBlocks);
-    datapacket.def("GetWaveformSamplesNBytes", &WaveformDataPacketPy::GetWaveformSamplesNBytes);
-    datapacket.def("GetWaveformNSamples", &WaveformDataPacketPy::GetWaveformNSamples);
-    datapacket.def("GetWaveformNBytes", &WaveformDataPacketPy::GetWaveformNBytes);
-    datapacket.def("GetWaveformStart", &WaveformDataPacketPy::GetWaveformStart);
-    datapacket.def("GetPacketNBytes", &WaveformDataPacketPy::GetPacketNBytes);
-    datapacket.def("CalculatePacketSizeBytes", &WaveformDataPacketPy::CalculatePacketSizeBytes);
-    datapacket.def("CalculateCellID", &WaveformDataPacketPy::CalculateCellID);
-    datapacket.def("CalculateRowColumnBlockPhase", &WaveformDataPacketPy::CalculateRowColumnBlockPhase);
-    datapacket.def("IsValid", &WaveformDataPacketPy::IsValid);
+    datapacket.def("GetDataPacket", &GetDataPacket, py::return_value_policy::reference_internal);
+    datapacket.def("GetPacketSize", &WaveformDataPacket::GetPacketSize);
+    datapacket.def("GetNWaveforms", &WaveformDataPacket::GetNWaveforms);
+    datapacket.def("GetRegisterSetup", &WaveformDataPacket::GetRegisterSetup);
+    datapacket.def("IsLastSubPacket", &WaveformDataPacket::IsLastSubPacket);
+    datapacket.def("IsFirstSubPacket", &WaveformDataPacket::IsFirstSubPacket);
+    datapacket.def("GetNBuffers", &WaveformDataPacket::GetNBuffers);
+    datapacket.def("GetTACKTime", &WaveformDataPacket::GetTACKTime);
+    datapacket.def("GetEventNumber", &WaveformDataPacket::GetEventNumber);
+    datapacket.def("GetSlotID", &WaveformDataPacket::GetSlotID);
+    datapacket.def("GetDetectorID", &WaveformDataPacket::GetDetectorID);
+    datapacket.def("GetEventSequenceNumber", &WaveformDataPacket::GetEventSequenceNumber);
+    datapacket.def("GetDetectorUniqueTag", &WaveformDataPacket::GetDetectorUniqueTag);
+    datapacket.def("GetColumn", &WaveformDataPacket::GetColumn);
+    datapacket.def("GetStaleBit", &WaveformDataPacket::GetStaleBit);
+    datapacket.def("IsZeroSupressionEnabled", &WaveformDataPacket::IsZeroSupressionEnabled);
+    datapacket.def("GetRow", &WaveformDataPacket::GetRow);
+    datapacket.def("GetBlockPhase", &WaveformDataPacket::GetBlockPhase);
+    datapacket.def("GetCRC", &WaveformDataPacket::GetCRC);
+    datapacket.def("GetMBZ", &WaveformDataPacket::GetMBZ);
+    datapacket.def("IsTimeout", &WaveformDataPacket::IsTimeout);
+    datapacket.def("IsError", &WaveformDataPacket::IsError);
+    datapacket.def("GetWaveformSamplesNBlocks", &WaveformDataPacket::GetWaveformSamplesNBlocks);
+    datapacket.def("GetWaveformSamplesNBytes", &WaveformDataPacket::GetWaveformSamplesNBytes);
+    datapacket.def("GetWaveformNSamples", &WaveformDataPacket::GetWaveformNSamples);
+    datapacket.def("GetWaveformNBytes", &WaveformDataPacket::GetWaveformNBytes);
+    datapacket.def("GetWaveformStart", &WaveformDataPacket::GetWaveformStart);
+    datapacket.def("GetPacketNBytes", &WaveformDataPacket::GetPacketNBytes);
+    datapacket.def("CalculatePacketSizeBytes", &WaveformDataPacket::CalculatePacketSizeBytes);
+    datapacket.def("CalculateCellID", &WaveformDataPacket::CalculateCellID);
+    datapacket.def("CalculateRowColumnBlockPhase", &WaveformDataPacket::CalculateRowColumnBlockPhase);
+    datapacket.def("IsValid", &WaveformDataPacket::IsValid);
 }
 
 }  // namespace interfaces
