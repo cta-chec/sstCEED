a5a851c52aedde0e79fad37ad3fb23b606a18d84
Jason Watson <jason.jw@live.co.uk>
2020-01-25 16:28:57 +0100

    Finish pybindings for WaveformEvent

--- interfaces/pybind/WaveformEvent.cc
+++ interfaces/pybind/WaveformEvent.cc
@@ -11,44 +11,6 @@ namespace interfaces {
 
 namespace py = pybind11;
 
-//template<typename T>
-//py::array_t<T> GetWaveforms(WaveformEvent<T>& event) {
-//    auto v = new std::vector<T>(event.GetWaveforms());
-//    auto capsule = py::capsule(v, [](void *v) { delete reinterpret_cast<std::vector<T>*>(v); });
-//    return py::array(v->size(), v->data(), capsule);
-//}
-
-template<typename T, typename TT>
-struct WaveformEventPy : public T {
-public:
-    using T::T;
-
-    py::array_t<TT> GetWaveformsPy() {
-        auto v = new std::vector<TT>(T::GetWaveforms());
-        auto capsule = py::capsule(v, [](void *v) { delete reinterpret_cast<std::vector<TT>*>(v); });
-        auto n_pixels = static_cast<long>(T::run_header->n_pixels);
-        auto n_samples = static_cast<long>(T::run_header->n_samples);
-        auto shape = std::vector<ptrdiff_t>{n_pixels, n_samples};
-        return py::array(shape, v->data(), capsule);
-    }
-};
-
-template<typename T>
-void waveform_event_t(py::module &m, const std::string& name) {
-    py::class_<T> waveform_event(m, name.c_str());
-    waveform_event.def(py::init<WaveformRunHeader*>());
-    waveform_event.def("GetWaveforms", &T::GetWaveformsPy);
-}
-
-void waveform_event(py::module &m) {
-    waveform_event_t<WaveformEventPy<WaveformEventR0, uint16_t>>(m, "WaveformEventR0");
-//    waveform_event_t<WaveformEventR1, float>(m, "WaveformEventR1");
-}
-
-//template<>
-//void waveform_event<WaveformEventR0>(py::module &m);
-
-
 void waveform_run_header(py::module &m) {
     py::class_<WaveformRunHeader> header(m, "WaveformRunHeader");
     header.def(py::init<size_t, size_t, std::set<uint8_t>&, size_t>());
@@ -65,7 +27,35 @@ void waveform_run_header(py::module &m) {
     header.def_readwrite("first_module_slot", &WaveformRunHeader::first_module_slot);
 }
 
+template<typename T, typename TT>
+py::array_t<TT> GetWaveforms(const T& waveform_event) {
+    auto v = new std::vector<TT>(waveform_event.GetWaveforms());
+    auto capsule = py::capsule(v, [](void *v) { delete reinterpret_cast<std::vector<TT>*>(v); });
+    auto n_pixels = static_cast<long>(waveform_event.run_header->n_pixels);
+    auto n_samples = static_cast<long>(waveform_event.run_header->n_samples);
+    auto shape = std::vector<ptrdiff_t>{n_pixels, n_samples};
+    return py::array(shape, v->data(), capsule);
+}
 
+template<typename T, typename TT>
+void waveform_event_template(py::module &m, const std::string& name) {
+    py::class_<T> waveform_event(m, name.c_str());
+    waveform_event.def(py::init<WaveformRunHeader*>());
+    waveform_event.def("GetWaveforms", GetWaveforms<T, TT>);
+    waveform_event.def("SetEventHeaderFromPackets", &T::SetEventHeaderFromPackets);
+    waveform_event.def_readonly("run_header", &T::run_header);
+    waveform_event.def_readonly("packets", &T::packets);
+    waveform_event.def_readonly("index", &T::index);
+    waveform_event.def_readonly("first_cell_id", &T::first_cell_id);
+    waveform_event.def_readonly("missing_packets", &T::missing_packets);
+    waveform_event.def_readonly("tack", &T::tack);
+    waveform_event.def_readonly("cpu_s", &T::cpu_s);
+    waveform_event.def_readonly("cpu_ns", &T::cpu_ns);
+}
 
-}  // namespace interfaces
-}  // namespace sstcam
+void waveform_event(py::module &m) {
+    waveform_event_template<WaveformEventR0, uint16_t>(m, "WaveformEventR0");
+    waveform_event_template<WaveformEventR1, float>(m, "WaveformEventR1");
+}
+
+}}
