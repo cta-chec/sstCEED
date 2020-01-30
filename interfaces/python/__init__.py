6395fb53a759f12c724378c6ae62e0e6b72ab0ef
Samuel <samuel.d.flis@gmail.com>
2020-01-30 21:08:09 +0100

    Simplify the python extension import

--- interfaces/python/__init__.py
+++ interfaces/python/__init__.py
@@ -1,5 +1,5 @@
 from sstcam import _import_module
 
-_m = _import_module(__name__, "sstcam_interfaces")
+_m = _import_module(__name__)
 from .ssreadout import SSReadout
 from .triggerpatternpacket import TriggerPacket
